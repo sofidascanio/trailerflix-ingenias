d59ff9376e2c99f01e801ae4d357ae4e54cb9c73
Sofia <sofiadascanio1@gmail.com>
2025-07-23 20:54:00 -0300

    config para deploy

--- trailerflix/src/mysql.js
+++ trailerflix/src/mysql.js
@@ -7,7 +7,7 @@ host = process.env.DB_HOST;
 database = process.env.DB_NAME;
 username = process.env.DB_USER;
 password = process.env.DB_PASS;
-db_port = process.env.DB_PORT;
+db_port = process.env.DB_PORT || 3306;
 
 const sequelize = new Sequelize (database, username, password, {
 	host: host,
